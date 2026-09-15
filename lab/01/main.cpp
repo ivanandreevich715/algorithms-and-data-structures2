07266dfa0a366aeba28209eaa4f49bb5e912e835
ivanandreevich715 <ivansradchenko521@gmail.com>
2026-09-15 19:10:06 +0300

    ex 11157 C++

---
+++ lab/01/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+void InsertionSort(int A[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int key = A[i];
+        int j = i - 1;
+
+        while (j >= 0 && A[j] > key)
+        {
+            A[j + 1] = A[j];
+            j--;
+        }
+
+        A[j + 1] = key;
+    }
+}
+
+int main()
+{
+    int A[1000];
+    int n = 0;
+
+    while (cin >> A[n])
+    {
+        n++;
+    }
+
+    InsertionSort(A, n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << A[i];
+
+        if (i < n - 1)
+            cout << " ";
+    }
+
+    return 0;
+}
